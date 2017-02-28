bbc5b2a3886cb12bcf97689516a5e91264e373f0
言志伟 <Yan.Zhiwei@hotmail.com>
2017-02-28 17:07:34 +0800

    sync git test

--- EFGetStarted.ConsoleApp.RelationshipsOne-to-one/Migrations/MyContextModelSnapshot.cs
+++ EFGetStarted.ConsoleApp.RelationshipsOne-to-one/Migrations/MyContextModelSnapshot.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using EFGetStarted.ConsoleApp.RelationshipsOne_to_one;
 
 namespace EFGetStarted.ConsoleApp.RelationshipsOne_to_one.Migrations
 {
@@ -13,47 +10,35 @@ namespace EFGetStarted.ConsoleApp.RelationshipsOne_to_one.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
             modelBuilder
-                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
+            .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+            .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
             modelBuilder.Entity("EFGetStarted.ConsoleApp.RelationshipsOne_to_one.Blog", b =>
-                {
-                    b.Property<int>("BlogId")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("Url");
-
-                    b.HasKey("BlogId");
-
-                    b.ToTable("Blogs");
-                });
-
+            {
+                b.Property<int>("BlogId")
+                .ValueGeneratedOnAdd();
+                b.Property<string>("Url");
+                b.HasKey("BlogId");
+                b.ToTable("Blogs");
+            });
             modelBuilder.Entity("EFGetStarted.ConsoleApp.RelationshipsOne_to_one.BlogImage", b =>
-                {
-                    b.Property<int>("BlogImageId")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<int>("BlogForeignKey");
-
-                    b.Property<string>("Caption");
-
-                    b.Property<byte[]>("Image");
-
-                    b.HasKey("BlogImageId");
-
-                    b.HasIndex("BlogForeignKey")
-                        .IsUnique();
-
-                    b.ToTable("BlogImages");
-                });
-
+            {
+                b.Property<int>("BlogImageId")
+                .ValueGeneratedOnAdd();
+                b.Property<int>("BlogForeignKey");
+                b.Property<string>("Caption");
+                b.Property<byte[]>("Image");
+                b.HasKey("BlogImageId");
+                b.HasIndex("BlogForeignKey")
+                .IsUnique();
+                b.ToTable("BlogImages");
+            });
             modelBuilder.Entity("EFGetStarted.ConsoleApp.RelationshipsOne_to_one.BlogImage", b =>
-                {
-                    b.HasOne("EFGetStarted.ConsoleApp.RelationshipsOne_to_one.Blog", "Blog")
-                        .WithOne("BlogImage")
-                        .HasForeignKey("EFGetStarted.ConsoleApp.RelationshipsOne_to_one.BlogImage", "BlogForeignKey")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
+            {
+                b.HasOne("EFGetStarted.ConsoleApp.RelationshipsOne_to_one.Blog", "Blog")
+                .WithOne("BlogImage")
+                .HasForeignKey("EFGetStarted.ConsoleApp.RelationshipsOne_to_one.BlogImage", "BlogForeignKey")
+                .OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 }
