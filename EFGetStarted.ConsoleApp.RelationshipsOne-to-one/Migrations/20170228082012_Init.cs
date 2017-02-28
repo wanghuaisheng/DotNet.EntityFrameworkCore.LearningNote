75929ac8ba2b653912830db2306d63ab1fce9f7e
言志伟 <Yan.Zhiwei@hotmail.com>
2017-02-28 17:06:47 +0800

    coding

--- EFGetStarted.ConsoleApp.RelationshipsOne-to-one/Migrations/20170228082012_Init.cs
+++ EFGetStarted.ConsoleApp.RelationshipsOne-to-one/Migrations/20170228082012_Init.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace EFGetStarted.ConsoleApp.RelationshipsOne_to_one.Migrations
 {
@@ -12,49 +10,46 @@ namespace EFGetStarted.ConsoleApp.RelationshipsOne_to_one.Migrations
             migrationBuilder.CreateTable(
                 name: "Blogs",
                 columns: table => new
-                {
-                    BlogId = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Url = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
-                });
-
+            {
+                BlogId = table.Column<int>(nullable: false)
+                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                         Url = table.Column<string>(nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Blogs", x => x.BlogId);
+            });
             migrationBuilder.CreateTable(
                 name: "BlogImages",
                 columns: table => new
-                {
-                    BlogImageId = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    BlogForeignKey = table.Column<int>(nullable: false),
-                    Caption = table.Column<string>(nullable: true),
-                    Image = table.Column<byte[]>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_BlogImages", x => x.BlogImageId);
-                    table.ForeignKey(
-                        name: "FK_BlogImages_Blogs_BlogForeignKey",
-                        column: x => x.BlogForeignKey,
-                        principalTable: "Blogs",
-                        principalColumn: "BlogId",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
+            {
+                BlogImageId = table.Column<int>(nullable: false)
+                              .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                              BlogForeignKey = table.Column<int>(nullable: false),
+                              Caption = table.Column<string>(nullable: true),
+                              Image = table.Column<byte[]>(nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_BlogImages", x => x.BlogImageId);
+                table.ForeignKey(
+                    name: "FK_BlogImages_Blogs_BlogForeignKey",
+                    column: x => x.BlogForeignKey,
+                    principalTable: "Blogs",
+                    principalColumn: "BlogId",
+                    onDelete: ReferentialAction.Cascade);
+            });
             migrationBuilder.CreateIndex(
                 name: "IX_BlogImages_BlogForeignKey",
                 table: "BlogImages",
                 column: "BlogForeignKey",
                 unique: true);
         }
-
+        
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "BlogImages");
-
             migrationBuilder.DropTable(
                 name: "Blogs");
         }
