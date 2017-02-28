ffd3730109412c8e8ebb2402be63642f4b42deb4
言志伟 <Yan.Zhiwei@hotmail.com>
2017-02-28 13:53:01 +0800

    coding

--- EFGetStarted.ConsoleApp/Program.cs
+++ EFGetStarted.ConsoleApp/Program.cs
@@ -1,14 +1,102 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EFGetStarted.ConsoleApp
 {
     public class Program
     {
+        //Add-Migration MyFirstMigration
+        //Update-Database
         public static void Main(string[] args)
         {
+            using(var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                
+                foreach(var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
+        }
+    }
+    
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs
+        {
+            get;
+            set;
+        }
+        
+        public DbSet<Post> Posts
+        {
+            get;
+            set;
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-N3GTH4E\\SQLEXPRESS;Database=EFGetStarted.ConsoleApp.NewDb;uid=sa;pwd=sasa;");
+        }
+    }
+    
+    public class Blog
+    {
+        public int BlogId
+        {
+            get;
+            set;
+        }
+        
+        public string Url
+        {
+            get;
+            set;
+        }
+        
+        public List<Post> Posts
+        {
+            get;
+            set;
+        }
+    }
+    
+    public class Post
+    {
+        public int PostId
+        {
+            get;
+            set;
+        }
+        
+        public string Title
+        {
+            get;
+            set;
+        }
+        
+        public string Content
+        {
+            get;
+            set;
+        }
+        
+        public int BlogId
+        {
+            get;
+            set;
+        }
+        
+        public Blog Blog
+        {
+            get;
+            set;
         }
     }
 }
