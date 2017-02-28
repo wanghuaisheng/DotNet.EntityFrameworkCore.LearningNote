06eaa319a47c2b5fd3fdd597c3573a87d093a4a4
言志伟 <Yan.Zhiwei@hotmail.com>
2017-02-28 13:53:57 +0800

    coding

---
+++ EFGetStarted.ConsoleApp.ExistingDb/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EFGetStarted.ConsoleApp.ExistingDb")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1decb4b-1ee0-4eb8-bd97-96e55c1f865d")]
