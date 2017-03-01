95389632da95bc16f6e8ecf95049dbf41fb6d721
言志伟 <Yan.Zhiwei@hotmail.com>
2017-03-01 10:09:15 +0800

    coding

---
+++ EFGetStarted.ConsoleApp.Inheritance/Migrations/MyContextModelSnapshot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EFGetStarted.ConsoleApp.Inheritance;
+
+namespace EFGetStarted.ConsoleApp.Inheritance.Migrations
+{
+    [DbContext(typeof(MyContext))]
+    partial class MyContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("EFGetStarted.ConsoleApp.Inheritance.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Discriminator")
+                        .IsRequired();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+
+                    b.HasDiscriminator<string>("Discriminator").HasValue("Blog");
+                });
+
+            modelBuilder.Entity("EFGetStarted.ConsoleApp.Inheritance.RssBlog", b =>
+                {
+                    b.HasBaseType("EFGetStarted.ConsoleApp.Inheritance.Blog");
+
+                    b.Property<string>("RssUrl");
+
+                    b.ToTable("RssBlog");
+
+                    b.HasDiscriminator().HasValue("RssBlog");
+                });
+        }
+    }
+}
---
+++ EFGetStarted.ConsoleApp.Inheritance/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted.ConsoleApp.Inheritance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+    
+    internal class MyContext : DbContext
+    {
+        public DbSet<Blog> Blogs
+        {
+            get;
+            set;
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-N3GTH4E\\SQLEXPRESS;Database=EFGetStarted.ConsoleApp.Inheritance;uid=sa;pwd=sasa;");
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RssBlog>();
+        }
+    }
+    
+    public class Blog
+    {
+        public int BlogId
+        {
+            get;
+            set;
+        }
+        
+        public string Url
+        {
+            get;
+            set;
+        }
+    }
+    
+    public class RssBlog : Blog
+    {
+        public string RssUrl
+        {
+            get;
+            set;
+        }
+    }
+}
